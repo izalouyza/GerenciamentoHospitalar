25e2675417e4098907a2a115697cc0505d6f984f
alexb7z <alexbrunoduarte@gmail.com>
2025-12-01 13:22:46 -0300

    Alteração da classe Paciente para receber os Exceptions

--- src/br/com/hospital/entidades/Paciente.java
+++ src/br/com/hospital/entidades/Paciente.java
@@ -1,6 +1,8 @@
 package br.com.hospital.entidades;
 
 import br.com.hospital.interfaces.Validavel;
+import br.com.hospital.exceptions.PacienteException;
+import br.com.hospital.utilitarios.Utilitarios;
 
 public class Paciente extends Pessoa implements Validavel {
     private int idade;
@@ -8,8 +10,40 @@ public class Paciente extends Pessoa implements Validavel {
 
     public Paciente(int id, String nome, String cpf, String telefone, String email, String endereco,String senha, String nivelAcesso, int idade, String historicoClinico) {
         super(id, nome, cpf, telefone, email, endereco,senha,nivelAcesso);
+
+        //Exceções:
+        try {
+            if (nome == null || nome.isBlank()) {
+                throw new PacienteException("Nome inválido.");
+            }
+            if (!Utilitarios.cpfValido(cpf)) {
+                throw new PacienteException("CPF inválido.");
+            }
+            if (!Utilitarios.telefoneValido(telefone)) {
+                throw new PacienteException("Telefone inválido.");
+            }
+            if (!Utilitarios.emailValido(email)) {
+                throw new PacienteException("E-mail inválido.");
+            }
+            if (endereco == null || endereco.isBlank()) {
+                throw new PacienteException("Endereço não pode ser vazio.");
+            }
+            if (senha == null || senha.length() < 4) {
+                throw new PacienteException("Senha inválida. Mínimo de 4 caracteres.");
+            }
+            if (idade <= 0 || idade > 120) {
+                throw new PacienteException("Idade inválida.");
+            }
+            if (historicoClinico == null || historicoClinico.isBlank()) {
+                throw new PacienteException("Histórico clínico inválido.");
+            }
+
         this.idade = idade;
         this.historicoClinico = historicoClinico;
+
+        } catch (PacienteException e) {
+            System.out.println("Erro ao criar paciente: " + e.getMessage());
+        }
     }
 
     @Override
