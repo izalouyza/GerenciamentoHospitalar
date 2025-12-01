842905d0a7a7b108aabb8c3e49752b28cfa9c094
alexb7z <alexbrunoduarte@gmail.com>
2025-12-01 13:52:19 -0300

    Alteração da classe Funcionario para receber os Exceptions

--- src/br/com/hospital/entidades/Funcionario.java
+++ src/br/com/hospital/entidades/Funcionario.java
@@ -1,5 +1,8 @@
 package br.com.hospital.entidades;
 
+import br.com.hospital.exceptions.FuncionarioException;
+import br.com.hospital.utilitarios.Utilitarios;
+
 public class Funcionario extends Pessoa {
     private String cargo;
     private String setor;
@@ -7,8 +10,42 @@ public class Funcionario extends Pessoa {
 
     public Funcionario(int id, String nome, String cpf, String telefone, String email, String endereco,String senha, String nivelAcesso, String cargo, String setor) {
         super(id, nome, cpf, telefone, email, endereco, senha, nivelAcesso);
+
+        //Exceções:
+        try {
+            if (cargo == null || cargo.isBlank()) {
+                throw new FuncionarioException("Cargo não pode ser vazio.");
+            }
+            if (setor == null || setor.isBlank()) {
+                throw new FuncionarioException("Setor não pode ser vazio.");
+            }
+            if (getNome() == null || getNome().isBlank()) {
+                throw new FuncionarioException("Nome inválido.");
+            }
+            if (!Utilitarios.cpfValido(getCpf())) {
+                throw new FuncionarioException("CPF inválido.");
+            }
+            if (!Utilitarios.telefoneValido(getTelefone())) {
+                throw new FuncionarioException("Telefone inválido.");
+            }
+            if (!Utilitarios.emailValido(getEmail())) {
+                throw new FuncionarioException("Email inválido.");
+            }
+
+            // ----- Nível de Acesso -----
+            if (nivelAcesso == null || nivelAcesso.isBlank()) {
+                throw new FuncionarioException("Nível de acesso inválido.");
+            }
+            if (!nivelAcesso.matches("ADMIN|MEDICO|RECEPCIONISTA|FUNCIONARIO")) {
+                throw new FuncionarioException("Nível de acesso não permitido.");
+            }
+
         this.cargo = cargo;
         this.setor = setor;
+
+        } catch (FuncionarioException e) {
+            System.out.println("Erro ao criar funcionário: " + e.getMessage());
+        }
     }
 
     //getters e setters
