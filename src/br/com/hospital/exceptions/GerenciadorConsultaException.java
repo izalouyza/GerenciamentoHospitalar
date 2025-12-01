560cf85bcdea547150cdab76498274459007ae07
alexb7z <alexbrunoduarte@gmail.com>
2025-12-01 13:06:54 -0300

    Criação dos arquivos exception para GerenciadorConsulta e Utilitarios

---
+++ src/br/com/hospital/exceptions/GerenciadorConsultaException.java
@@ -0,0 +1,7 @@
+package br.com.hospital.exceptions;
+
+public class GerenciadorConsultaException extends Exception {
+    public GerenciadorConsultaException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/br/com/hospital/exceptions/UtilitariosException.java
@@ -0,0 +1,7 @@
+package br.com.hospital.exceptions;
+
+public class UtilitariosException extends Exception {
+    public UtilitariosException(String msg) {
+        super(msg);
+    }
+}
