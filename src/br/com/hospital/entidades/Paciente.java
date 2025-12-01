6094dece620ba1164eaef4bff48fd48721f8f180
alexb7z <alexbrunoduarte@gmail.com>
2025-12-01 14:21:05 -0300

    Correção nas exceções da classe Paciente

--- src/br/com/hospital/entidades/Paciente.java
+++ src/br/com/hospital/entidades/Paciente.java
@@ -8,42 +8,52 @@ public class Paciente extends Pessoa implements Validavel {
     private int idade;
     private String historicoClinico;
 
-    public Paciente(int id, String nome, String cpf, String telefone, String email, String endereco,String senha, String nivelAcesso, int idade, String historicoClinico) {
-        super(id, nome, cpf, telefone, email, endereco,senha,nivelAcesso);
+    public Paciente(int id, String nome, String cpf, String telefone, String email, String endereco,String senha, String nivelAcesso, int idade, String historicoClinico)
+        throws PacienteException {
 
         //Exceções:
-        try {
-            if (nome == null || nome.isBlank()) {
-                throw new PacienteException("Nome inválido.");
-            }
-            if (!Utilitarios.cpfValido(cpf)) {
-                throw new PacienteException("CPF inválido.");
-            }
-            if (!Utilitarios.telefoneValido(telefone)) {
-                throw new PacienteException("Telefone inválido.");
-            }
-            if (!Utilitarios.emailValido(email)) {
-                throw new PacienteException("E-mail inválido.");
-            }
-            if (endereco == null || endereco.isBlank()) {
-                throw new PacienteException("Endereço não pode ser vazio.");
-            }
-            if (senha == null || senha.length() < 4) {
-                throw new PacienteException("Senha inválida. Mínimo de 4 caracteres.");
-            }
-            if (idade <= 0 || idade > 120) {
-                throw new PacienteException("Idade inválida.");
-            }
-            if (historicoClinico == null || historicoClinico.isBlank()) {
-                throw new PacienteException("Histórico clínico inválido.");
-            }
+        if (!Utilitarios.textoNaoVazio(nome)) {
+            throw new PacienteException("Nome inválido.");
+        }
+
+        if (!Utilitarios.cpfValido(cpf)) {
+            throw new PacienteException("CPF inválido.");
+        }
+
+        if (!Utilitarios.telefoneValido(telefone)) {
+            throw new PacienteException("Telefone inválido.");
+        }
+
+        if (!Utilitarios.emailValido(email)) {
+            throw new PacienteException("E-mail inválido.");
+        }
+
+        if (!Utilitarios.textoNaoVazio(endereco)) {
+            throw new PacienteException("Endereço inválido.");
+        }
+
+        if (!Utilitarios.textoNaoVazio(senha) || senha.length() < 4) {
+            throw new PacienteException("Senha inválida. Mínimo de 4 caracteres.");
+        }
+
+        if (!Utilitarios.textoNaoVazio(nivelAcesso)) {
+            throw new PacienteException("Nível de acesso inválido.");
+        }
+
+        if (idade <= 0 || idade > 120) {
+            throw new PacienteException("Idade inválida.");
+        }
+
+        if (!Utilitarios.textoNaoVazio(historicoClinico)) {
+            throw new PacienteException("Histórico clínico inválido.");
+        }
+
+        super(id, nome, cpf, telefone, email, endereco,senha,nivelAcesso);
+
 
         this.idade = idade;
         this.historicoClinico = historicoClinico;
 
-        } catch (PacienteException e) {
-            System.out.println("Erro ao criar paciente: " + e.getMessage());
-        }
     }
 
     @Override
