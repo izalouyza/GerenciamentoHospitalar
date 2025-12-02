2e02b88eb8f0b315b9ff23d340fba0ff33c9c624
livianlucena <livianmarialucena@gmail.com>
2025-12-02 14:57:47 -0300

    Mudança da lógica das funções de login

--- src/br/com/hospital/sistema/Login.java
+++ src/br/com/hospital/sistema/Login.java
@@ -1,55 +1,38 @@
 package br.com.hospital.sistema;
 
+import br.com.hospital.entidades.Funcionario;
 import br.com.hospital.entidades.Pessoa;
 import br.com.hospital.exceptions.LoginException;
 
 import java.util.List;
 
 public class Login {
-
-    private List<Pessoa> usuarios;
+    private final List<Pessoa> usuarios;
     private Pessoa usuarioLogado;
 
     public Login(List<Pessoa> usuarios) {
         this.usuarios = usuarios;
     }
 
+    // autentica apenas funcionários
     public boolean autenticar(String cpf, String senha) throws LoginException {
-        if (cpf == null || senha == null) {
-            throw new LoginException("CPF ou senha não podem ser nulos.");
-        }
-
         for (Pessoa p : usuarios) {
-            if (p.getCpf().equals(cpf) && p.getSenha().equals(senha)) {
+            if (p instanceof Funcionario &&
+                    p.getCpf().equals(cpf) &&
+                    p.getSenha().equals(senha)) {
                 usuarioLogado = p;
                 return true;
             }
         }
-
-        throw new LoginException("CPF ou senha incorretos!");
+        throw new LoginException("CPF ou senha incorretos (somente funcionário pode logar).");
     }
 
     public Pessoa getUsuarioLogado() {
         return usuarioLogado;
     }
-
-    public String getNivelAcesso() {
-        if (usuarioLogado != null) {
-            return usuarioLogado.getNivelAcesso();
-        }
-
-        return null;
-    }
-
-    public boolean temPermissao(String acao) {
-        if (usuarioLogado != null) {
-            return usuarioLogado.temPermissao(acao);
-        }
-
-        return false;
-    }
-
     public void logout() {
         usuarioLogado = null;
     }
 }
+
+
