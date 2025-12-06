df6a4d0399726c2b17d4ad4bef7fef51dd00c0f3
izalouyza <izadora.figueiredo@alunos.ufersa.edu.br>
2025-12-06 14:32:50 -0300

    Correções de parâmetros, getters e setters e adicionar validação

--- src/br/com/hospital/entidades/Paciente.java
+++ src/br/com/hospital/entidades/Paciente.java
@@ -1,16 +1,21 @@
 package br.com.hospital.entidades;
 
+import br.com.hospital.interfaces.Validavel;
 import br.com.hospital.utilitarios.Utilitarios;
 
-public class Paciente extends Pessoa {
+public class Paciente extends Pessoa implements Validavel {
+
     private int idade;
     private String principalQueixa;
 
+    private String mensagemValidacao = "";
+
     public Paciente(String id, String nome, String cpf, String telefone, String email,
-                    String endereco, String senha, int idade, String principalQueixao) {
+                    String endereco, String senha, int idade, String principalQueixa) {
+
         super(id, nome, cpf, telefone, email, endereco, senha, "PACIENTE");
         this.idade = idade;
-        this.principalQueixa = principalQueixa;
+        this.principalQueixa = Utilitarios.normalizarTexto(principalQueixa);
     }
 
     public int getIdade() {
@@ -21,12 +26,12 @@ public class Paciente extends Pessoa {
         this.idade = idade;
     }
 
-    public String getprincipalQueixa() {
+    public String getPrincipalQueixa() {
         return principalQueixa;
     }
 
-    public void setprincipalQueixa(String principalQueixa) {
-        this.principalQueixa = principalQueixa;
+    public void setPrincipalQueixa(String principalQueixa) {
+        this.principalQueixa = Utilitarios.normalizarTexto(principalQueixa);
     }
 
     @Override
@@ -34,5 +39,33 @@ public class Paciente extends Pessoa {
         super.exibirInformacoes();
         Utilitarios.println("Idade: " + idade);
         Utilitarios.println("Principal queixa: " + principalQueixa);
+    }
+
+    // Implementação do Validavel
+    @Override
+    public boolean validar() {
+
+        if (!super.validar()) {
+            mensagemValidacao = super.getMensagemValidacao();
+            return false;
+        }
+
+        if (idade <= 0) {
+            mensagemValidacao = "A idade deve ser maior que zero.";
+            return false;
+        }
+
+        if (principalQueixa == null || principalQueixa.isBlank()) {
+            mensagemValidacao = "A principal queixa não pode ser vazia.";
+            return false;
+        }
+
+        mensagemValidacao = "";
+        return true;
+    }
+
+    @Override
+    public String getMensagemValidacao() {
+        return mensagemValidacao;
     }
 }
