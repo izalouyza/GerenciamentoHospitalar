6835ba76037de3db22bc4339b0d3a63f4d5007f1
livianlucena <livianmarialucena@gmail.com>
2025-12-06 01:06:13 -0300

    Alteração da função exibirResumo e organização na classe Consulta

--- src/br/com/hospital/entidades/Consulta.java
+++ src/br/com/hospital/entidades/Consulta.java
@@ -7,47 +7,52 @@ public class Consulta {
     private Paciente paciente;
     private Medico medico;
     private String dataHora; // formato dd/MM/yyyy HH:mm
-    private String descricao;
 
-    public Consulta(String id, Paciente paciente, Medico medico, String dataHora, String descricao) {
+    public Consulta(String id, Paciente paciente, Medico medico, String dataHora) {
         this.id = id;
         this.paciente = paciente;
         this.medico = medico;
         this.dataHora = dataHora;
-        this.descricao = Utilitarios.normalizarTexto(descricao);
     }
 
     public String getId() {
         return id;
     }
+
     public Paciente getPaciente() {
         return paciente;
     }
+
     public Medico getMedico() {
         return medico;
     }
+
     public String getDataHora() {
         return dataHora;
-    }
-    public String getDescricao() {
-        return descricao;
     }
 
     public void setPaciente(Paciente paciente) {
         this.paciente = paciente;
     }
+
     public void setMedico(Medico medico) {
         this.medico = medico;
     }
+
     public void setDataHora(String dataHora) {
         this.dataHora = dataHora;
     }
-    public void setDescricao(String descricao) {
-        this.descricao = Utilitarios.normalizarTexto(descricao);
-    }
+
 
     public void exibirResumo() {
-        System.out.printf("Consulta %s | Paciente: %s | Médico: %s | %s | %s%n",
-                id, paciente.getNome(), medico.getNome(), dataHora, descricao);
+        System.out.printf("""
+                             Dados da consulta:
+                             Protocolo: %s
+                             Paciente: %s
+                             Médico: %s
+                             Data: %s
+                             Dara: %s
+                             Queixa do paciente: %s""",
+                id, paciente.getNome(), medico.getNome(), dataHora, paciente.getprincipalQueixa());
     }
 }
