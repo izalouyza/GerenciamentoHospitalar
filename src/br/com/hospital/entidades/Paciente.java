8434d7ab81eff430cc94a472bb344ab5014d6ee4
livianlucena <livianmarialucena@gmail.com>
2025-12-06 00:46:20 -0300

    Mudança da variavel historicoClinico para principalQueixa e organização da classe Paciente

--- src/br/com/hospital/entidades/Paciente.java
+++ src/br/com/hospital/entidades/Paciente.java
@@ -4,32 +4,35 @@ import br.com.hospital.utilitarios.Utilitarios;
 
 public class Paciente extends Pessoa {
     private int idade;
-    private String historicoClinico;
+    private String principalQueixa;
 
     public Paciente(String id, String nome, String cpf, String telefone, String email,
-                    String endereco, String senha, int idade, String historicoClinico) {
+                    String endereco, String senha, int idade, String principalQueixao) {
         super(id, nome, cpf, telefone, email, endereco, senha, "PACIENTE");
         this.idade = idade;
-        this.historicoClinico = Utilitarios.normalizarTexto(historicoClinico);
+        this.principalQueixa = principalQueixa;
     }
 
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    public String getHistoricoClinico() {
-        return historicoClinico;
+
+    public String getprincipalQueixa() {
+        return principalQueixa;
     }
-    public void setHistoricoClinico(String historicoClinico) {
-        this.historicoClinico = Utilitarios.normalizarTexto(historicoClinico);
+
+    public void setprincipalQueixa(String principalQueixa) {
+        this.principalQueixa = principalQueixa;
     }
 
     @Override
     public void exibirInformacoes() {
         super.exibirInformacoes();
         Utilitarios.println("Idade: " + idade);
-        Utilitarios.println("Histórico Clínico: " + historicoClinico);
+        Utilitarios.println("Principal queixa: " + principalQueixa);
     }
 }
