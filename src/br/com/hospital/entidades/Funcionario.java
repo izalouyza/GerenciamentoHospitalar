0722c7fe9021c8ffaada243b327daa7e621e1f33
livianlucena <livianmarialucena@gmail.com>
2025-12-06 00:54:22 -0300

    Organização na classe Funcionario e adição de sugestão

--- src/br/com/hospital/entidades/Funcionario.java
+++ src/br/com/hospital/entidades/Funcionario.java
@@ -2,13 +2,14 @@ package br.com.hospital.entidades;
 
 import br.com.hospital.utilitarios.Utilitarios;
 
+//Trocar essa classe para Admin, a qual sera herdada pela classe Secretaria
+
 public class Funcionario extends Pessoa {
     private String cargo;
     private String setor;
 
     public Funcionario(String id, String nome, String cpf, String telefone, String email,
-                       String endereco, String senha, String nivelAcesso,
-                       String cargo, String setor) {
+                       String endereco, String senha, String nivelAcesso, String cargo, String setor) {
         super(id, nome, cpf, telefone, email, endereco, senha, nivelAcesso);
         this.cargo = cargo;
         this.setor = setor;
@@ -17,9 +18,11 @@ public class Funcionario extends Pessoa {
     public String getCargo() {
         return cargo;
     }
+
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
+
     public String getSetor() {
         return setor;
     }
