a4690e9343a7f20248036773e2bd5aa9244a504a
izalouyza <izadora.figueiredo@alunos.ufersa.edu.br>
2025-12-06 14:29:20 -0300

    Implementa validações específicas em Funcionario e integração com validação herdada de Pessoa

--- src/br/com/hospital/entidades/Funcionario.java
+++ src/br/com/hospital/entidades/Funcionario.java
@@ -2,39 +2,86 @@ package br.com.hospital.entidades;
 
 import br.com.hospital.utilitarios.Utilitarios;
 
-//Trocar essa classe para Admin, a qual sera herdada pela classe Secretaria
-
 public class Funcionario extends Pessoa {
+
     private String cargo;
     private String setor;
 
+    private String mensagemValidacao = "";
+
     public Funcionario(String id, String nome, String cpf, String telefone, String email,
-                       String endereco, String senha, String nivelAcesso, String cargo, String setor) {
+                       String endereco, String senha, String nivelAcesso,
+                       String cargo, String setor) {
+
         super(id, nome, cpf, telefone, email, endereco, senha, nivelAcesso);
         this.cargo = cargo;
         this.setor = setor;
     }
 
+    // ------------------- GETTERS E SETTERS ---------------------
+
     public String getCargo() {
         return cargo;
     }
 
-    public void setCargo(String cargo) {
+    public boolean setCargo(String cargo) {
+        if (cargo == null || cargo.isBlank()) {
+            mensagemValidacao = "O cargo não pode ser vazio.";
+            return false;
+        }
         this.cargo = cargo;
+        return true;
     }
 
     public String getSetor() {
         return setor;
     }
-    public void setSetor(String setor) {
+
+    public boolean setSetor(String setor) {
+        if (setor == null || setor.isBlank()) {
+            mensagemValidacao = "O setor não pode ser vazio.";
+            return false;
+        }
         this.setor = setor;
+        return true;
     }
+
+    // ------------------ PRINT ---------------------
 
     @Override
     public void exibirInformacoes() {
         super.exibirInformacoes();
         Utilitarios.println("Cargo: " + cargo);
         Utilitarios.println("Setor: " + setor);
-        
+    }
+
+    // -----------------------------------------------------
+    //               MÉTODOS DE VALIDAÇÃO
+    // -----------------------------------------------------
+    @Override
+    public boolean validar() {
+
+        if (!super.validar()) {
+            mensagemValidacao = super.getMensagemValidacao();
+            return false;
+        }
+
+        if (cargo == null || cargo.isBlank()) {
+            mensagemValidacao = "O cargo não pode ser vazio.";
+            return false;
+        }
+
+        if (setor == null || setor.isBlank()) {
+            mensagemValidacao = "O setor não pode ser vazio.";
+            return false;
+        }
+
+        mensagemValidacao = "";
+        return true;
+    }
+
+    @Override
+    public String getMensagemValidacao() {
+        return mensagemValidacao;
     }
 }
