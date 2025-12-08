6a5cb8768ebfd5ecd57795b9e3b7c465e37e0da7
livianlucena <livianmarialucena@gmail.com>
2025-12-08 18:21:01 -0300

    Edição da ordem dos atributos no cadastro do médico

--- src/br/com/hospital/gerenciadores/GerenciadorMedico.java
+++ src/br/com/hospital/gerenciadores/GerenciadorMedico.java
@@ -51,6 +51,12 @@ public class GerenciadorMedico implements Gerenciavel<Medico> {
         Print("Endereço: ");
         String endereco = sc.nextLine();
 
+        // CRM
+        String crm = lerCRM();
+
+        // Especialidade
+        String especialidade = lerCampoObrigatorio("Especialidade");
+
         // LOGIN DO MÉDICO
         Print("Usuário para login: ");
         String usuario = sc.nextLine();
@@ -65,12 +71,6 @@ public class GerenciadorMedico implements Gerenciavel<Medico> {
 
         // registra esse usuário na lista usada pelo Login
         usuariosSistema.add(credenciais);
-
-        // CRM
-        String crm = lerCRM();
-
-        // Especialidade
-        String especialidade = lerCampoObrigatorio("Especialidade");
 
         // Criar objeto médico
         Medico medico = new Medico(
@@ -294,19 +294,19 @@ public class GerenciadorMedico implements Gerenciavel<Medico> {
 
     private String lerEmail() {
         while (true) {
-            Print("Email: ");
+            Print("E-mail: ");
             String e = sc.nextLine();
             if (emailValido(e)) return e;
-            Println("Email inválido!");
+            Println("E-mail inválido!");
         }
     }
 
     private String lerEmailOpcional(String atual) {
-        Print("Novo email (atual: " + atual + "): ");
+        Print("Novo e-mail (atual: " + atual + "): ");
         String e = sc.nextLine();
         if (!textoNaoVazio(e)) return atual;
         if (!emailValido(e)) {
-            Println("Email inválido.");
+            Println("E-mail inválido.");
             return atual;
         }
         return e;
