61914b274945b23aa0e4847a527052c861419dc3
livianlucena <livianmarialucena@gmail.com>
2025-12-09 04:46:39 -0300

    Adição da função para formatar cpf

--- src/br/com/hospital/utilitarios/Utilitarios.java
+++ src/br/com/hospital/utilitarios/Utilitarios.java
@@ -292,5 +292,14 @@ public class Utilitarios {
                 Escolha uma opção: """);
     }
 
+    // Formata CPF: 12345678900 → 123.456.789-00
+    public static String formatarCpf(String cpf) {
+        if (cpf == null || cpf.length() != 11) return cpf;
+        return cpf.substring(0, 3) + "." +
+                cpf.substring(3, 6) + "." +
+                cpf.substring(6, 9) + "-" +
+                cpf.substring(9, 11);
+    }
+
 }
 
